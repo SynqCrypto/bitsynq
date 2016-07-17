7db6d068e21a0287bc22714dc205f0bb62d55774
Zaph3t <bitsynq@gmail.com>
2016-07-17 09:50:44 -0400

    Initial source commit v1.1
    BitSynq Qt source code upload v1.1

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitSYNQ-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitSYNQURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
